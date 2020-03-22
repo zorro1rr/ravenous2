5c6e7ce1dc6a0e580f534b3a06687873ed3b5f9f
Justin Howard <zorro1rr@gmail.com>
2020-03-21 19:53:19 -0600

    Updates

---
+++ precache-manifest.e5819c14b2c67683bbcfee50d6c5b2a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c20cd8da436688192bf26fba1ff4b8d",
+    "url": "/ravenous2/index.html"
+  },
+  {
+    "revision": "2b6c40bc2c5e4a3f75ed",
+    "url": "/ravenous2/static/css/main.3c3e4c5f.chunk.css"
+  },
+  {
+    "revision": "06844cd77216a4a3c32c",
+    "url": "/ravenous2/static/js/2.1ccc63ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous2/static/js/2.1ccc63ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6c40bc2c5e4a3f75ed",
+    "url": "/ravenous2/static/js/main.79cff46c.chunk.js"
+  },
+  {
+    "revision": "5ea92e567a839be9999e",
+    "url": "/ravenous2/static/js/runtime-main.e06702f6.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous2/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous2/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
